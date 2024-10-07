5f1a5add34ed7d41f102c8619e4a9e02383aaace
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2024-10-07 13:02:57 +0200

    delete exerciseDao

--- app/src/main/java/com/ivtogi/workouttimer/data/database/AppDatabase.kt
+++ app/src/main/java/com/ivtogi/workouttimer/data/database/AppDatabase.kt
@@ -2,7 +2,6 @@ package com.ivtogi.workouttimer.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.ivtogi.workouttimer.data.database.dao.ExerciseDao
 import com.ivtogi.workouttimer.data.database.dao.TimerDao
 import com.ivtogi.workouttimer.data.database.entities.Exercise
 import com.ivtogi.workouttimer.data.database.entities.Timer
@@ -10,5 +9,4 @@ import com.ivtogi.workouttimer.data.database.entities.Timer
 @Database(entities = [Timer::class, Exercise::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun timerDao(): TimerDao
-    abstract fun exerciseDao(): ExerciseDao
 }
--- app/src/main/java/com/ivtogi/workouttimer/data/database/dao/TimerDao.kt
+++ app/src/main/java/com/ivtogi/workouttimer/data/database/dao/TimerDao.kt
@@ -3,6 +3,8 @@ package com.ivtogi.workouttimer.data.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
+import com.ivtogi.workouttimer.data.database.entities.Exercise
 import com.ivtogi.workouttimer.data.database.entities.Timer
 import com.ivtogi.workouttimer.data.database.entities.TimerWithExercises
 
@@ -12,9 +14,23 @@ interface TimerDao {
     @Query("SELECT * FROM Timers WHERE id = :id")
     suspend fun getTimerById(id: Int): Timer
 
+    @Transaction
     @Query("SELECT * FROM Timers WHERE id = :id")
     suspend fun getTimerWithExercisesById(id: Int): TimerWithExercises
 
     @Insert
-    suspend fun insertTimer(timer: Timer)
+    suspend fun insertTimer(timer: Timer): Int
+
+    @Transaction
+    @Insert
+    suspend fun insertTimerWithExercises(timer: Timer, exercises: List<Exercise>){
+        val timerId = insertTimer(timer)
+        exercises.forEach { exercise -> insertExercise(exercise.copy(timerId = timerId)) }
+    }
+
+    @Query("SELECT * FROM exercises WHERE timerId = :timerId")
+    suspend fun getExercisesByTimerId(timerId: Int): List<Exercise>
+
+    @Insert
+    suspend fun insertExercise(exercise: Exercise)
 }
