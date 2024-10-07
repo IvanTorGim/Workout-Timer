33cf6bd9d94f8308c3c6a8da689de18193740706
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2024-10-07 17:48:55 +0200

    timerDao on conflict replace

--- app/src/main/java/com/ivtogi/workouttimer/data/database/dao/TimerDao.kt
+++ app/src/main/java/com/ivtogi/workouttimer/data/database/dao/TimerDao.kt
@@ -2,6 +2,7 @@ package com.ivtogi.workouttimer.data.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import com.ivtogi.workouttimer.data.database.entities.Exercise
@@ -18,19 +19,19 @@ interface TimerDao {
     @Query("SELECT * FROM Timers WHERE id = :id")
     suspend fun getTimerWithExercisesById(id: Int): TimerWithExercises
 
-    @Insert
-    suspend fun insertTimer(timer: Timer): Int
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTimer(timer: Timer): Long
 
     @Transaction
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTimerWithExercises(timer: Timer, exercises: List<Exercise>){
         val timerId = insertTimer(timer)
-        exercises.forEach { exercise -> insertExercise(exercise.copy(timerId = timerId)) }
+        exercises.forEach { exercise -> insertExercise(exercise.copy(timerId = timerId.toInt())) }
     }
 
     @Query("SELECT * FROM exercises WHERE timerId = :timerId")
     suspend fun getExercisesByTimerId(timerId: Int): List<Exercise>
 
-    @Insert
-    suspend fun insertExercise(exercise: Exercise)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExercise(exercise: Exercise): Long
 }
