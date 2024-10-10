94ff896f6e19dad6a81d459ee6d01c542baa2ffa
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2024-10-10 17:31:35 +0200

    refactor name and add variable finished

--- app/src/main/java/com/ivtogi/workouttimer/data/local/entities/TimerEntity.kt
+++ app/src/main/java/com/ivtogi/workouttimer/data/local/entities/TimerEntity.kt
@@ -3,10 +3,11 @@ package com.ivtogi.workouttimer.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Timers")
+@Entity(tableName = "timers")
 data class TimerEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val initial: Int = 0,
     val end: Int = 0,
-    val rounds: Int = 0
+    val rounds: Int = 0,
+    val finished: Boolean = false
 )
