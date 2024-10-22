35ae6a5841fdd638052aaba888a70bcf97c50129
Ivan Tortosa Gimeno <itortosagimeno@gmail.com>
2024-10-22 14:23:48 +0200

    app icon

--- app/src/main/java/com/itortosagimeno/workouttimer/ui/theme/Color.kt
+++ app/src/main/java/com/itortosagimeno/workouttimer/ui/theme/Color.kt
@@ -4,11 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Orange55 = Color(0xFFf9a822)
 val Orange80 = Color(0xFFfcd79c)
-val Orange90 = Color(0xFFfeebcd)
-val Yellow55 = Color(0xFFf0c828)
-val Yellow80 = Color(0xFFf8e7a0)
-val Blue55 = Color(0xFF2bbaa5)
-val Blue80 = Color(0xFFacece2)
 
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
